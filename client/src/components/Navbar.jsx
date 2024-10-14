33a46ad93d23644dff134a5fb6e6d3df3344b2e6
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-14 21:44:02 +0300

    remove login and register buttons from navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="shadow-md mb-4">
+    <nav className="shadow-md mb-4 text-white">
       <div className="max-w-6xl mx-auto p-3 flex flex-row justify-between items-center">
         <div className="text-xl">
           <Link to="/">
-            <span className="text-[#262626] font-bold">Gamma</span>
-            <span className="text-[#B3001B] font-bold">ridge</span>
+            <span className="text-[#eeeeee] font-bold">Gamma</span>
+            <span className="text-[#b9283b] font-bold">ridge</span>
           </Link>
         </div>
         <div className="flex flex-row gap-3 items-center">
@@ -21,7 +21,8 @@ function Navbar() {
               Contact
             </span>
           </Link>
-          <Link to={"/login"}>
+
+          {/* <Link to={"/login"}>
             <span className="text-[#255C99] font-semibold border-[#255C99] rounded-md border-2 px-3 py-2 hover:bg-slate-200">
               Login
             </span>
@@ -30,7 +31,7 @@ function Navbar() {
             <span className="text-white font-semibold bg-[#6D1321] rounded-md px-3 py-2 hover:opacity-80 border-2 border-[#6D1321]">
               Register
             </span>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </nav>
