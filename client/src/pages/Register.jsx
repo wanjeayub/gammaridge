06668fd753165177da2bc338797ae6345a5830bf
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-20 14:04:45 +0300

    update ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -7,12 +7,16 @@ const Register = () => {
     email: "",
     phone: "",
     alternatephone: "",
-    password1: "",
-    password2: "",
+    password: "",
+    agreedTerms: false,
   });
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
   };
 
   const handleSubmit = async (e) => {
@@ -24,9 +28,6 @@ const Register = () => {
       alert("The two phone numbers must be different.");
       return;
     }
-    // confirm passwords
-    if (formData.password1 != formData.password2)
-      alert("passwords not matching! check and try again");
 
     const response = await fetch("/api/users/register", {
       method: "POST",
@@ -36,8 +37,7 @@ const Register = () => {
     const data = await response.json();
 
     if (response.ok) {
-      // localStorage.setItem("token", data.token);
-      window.location.href = "/docs";
+      window.location.href = "/login";
     } else {
       alert(data.message);
     }
@@ -45,77 +45,96 @@ const Register = () => {
 
   return (
     <div className="p-4 text-white">
-      <div>
-        <div>Form content details</div>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-          <input
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Name"
-            required
-            className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
-          />
-          <input
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            placeholder="Mobile Number"
-            required
-            className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
-          />
-          <input
-            name="alternatephone"
-            value={formData.alternatephone}
-            onChange={handleChange}
-            placeholder="Alternate Mobile Number"
-            required
-            className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
-          />
-          <input
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Email"
-            required
-            className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
-          />
-          <input
-            name="password1"
-            type="password"
-            value={formData.password1}
-            onChange={handleChange}
-            placeholder="Password"
-            required
-            className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
-          />
-          <input
-            name="password2"
-            type="password"
-            value={formData.password2}
-            onChange={handleChange}
-            placeholder="confirm Password"
-            required
-            className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
-          />
-          <button
-            type="submit"
-            className="bg-[#b9283b] text-white py-2 px-4 rounded-md"
-          >
-            Register
-          </button>
-        </form>
-        <div>
-          <p className="max-w-md mx-auto space-y-4">
-            Already registered?{" "}
-            <Link to={"/login"}>
-              <span className="text-[#b9283b]">Login</span>
-            </Link>
-          </p>
-        </div>
-      </div>
+      <div className="flex md:flex-row ">
+        <div className="md:w-1/2 p-3">
+          <div className="p-3">
+            <p className="text-5xl mb-6">
+              Welcome to a <span className="text-[#b9283b]">platform</span> that
+              will open opportunities!
+            </p>
+            <p className="text-xl mb-2">
+              create an <span className="text-[#b9283b]">account</span> in
+              simple and easy steps
+            </p>
+          </div>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-3 p-2">
+            <input
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Name"
+              required
+              className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
+            />
+            <input
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              placeholder="Mobile Number"
+              required
+              className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
+            />
+            <input
+              name="alternatephone"
+              value={formData.alternatephone}
+              onChange={handleChange}
+              placeholder="Alternate Mobile Number"
+              required
+              className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
+            />
+            <input
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              required
+              className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
+            />
+            <input
+              name="password"
+              type="password"
+              value={formData.password1}
+              onChange={handleChange}
+              placeholder="Password"
+              required
+              className="bg-gray-600 text-white px-3 py-2 rounded-md border focus:outline-none focus:ring-0 focus:border-[#b9283b]"
+            />
 
-      <div>side</div>
+            <label
+              className="flex flex-row gap-3 items-center p-3"
+              htmlFor="agreedTerms"
+            >
+              <input
+                type="checkbox"
+                name="agreedTerms"
+                checked={formData.agreedTerms}
+                onChange={handleChange}
+              />
+              read our{" "}
+              <Link to={"/terms"}>
+                <span className="text-[#b9283b]">terms and conditions</span>
+              </Link>
+            </label>
+
+            <button
+              type="submit"
+              className="bg-[#b9283b] text-white py-2 px-4 rounded-md max-w-40"
+            >
+              Register
+            </button>
+          </form>
+          <div className="p-3">
+            <p className="">
+              Already registered?{" "}
+              <Link to={"/login"}>
+                <span className="text-[#b9283b]">Login</span>
+              </Link>
+            </p>
+          </div>
+        </div>
+        {/* side div with image */}
+        <div className="hidden md:block bg-yellow-900 md:w-1/2">side</div>
+      </div>
     </div>
   );
 };
