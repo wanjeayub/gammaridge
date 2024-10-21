da99242bc0b558fb2e79c2046e4be7df40c515e1
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-21 20:14:00 +0300

    adjust link to firebase sdk

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,5 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const storage = initializeApp(firebaseConfig);
-export default storage;
+const app = initializeApp(firebaseConfig);
+export default app;
