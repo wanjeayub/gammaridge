c417a2f90d635725f214e0b2e143d06fd6d03ff1
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-22 13:00:07 +0300

    Update Login.jsx remove localhost

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/users/login", {
+    const response = await fetch("/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
