c42b23c174a27ceb673d6b49bbaaf4af13a938b0
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-22 13:00:47 +0300

    Update AltRegister.jsx remove localhost

--- client/src/pages/AltRegister.jsx
+++ client/src/pages/AltRegister.jsx
@@ -73,7 +73,7 @@ const RegisterForm = () => {
       // log url
       console.log(photoURL);
       // Save data to backend (MongoDB)
-      const response = await fetch("http://localhost:5000/api/users/register", {
+      const response = await fetch("/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
