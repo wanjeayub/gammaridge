087a584ea5df2667b969f990fbbfe94a302223c8
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-31 17:29:54 +0300

    remove proxy configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://gammaridge-server.vercel.app",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-    cors: false,
-  },
   plugins: [react()],
 });
+
+//   server: {
+//   proxy: {
+//     "/api": {
+//       target: "https://gammaridge-server.vercel.app",
+//       changeOrigin: true,
+//       secure: false,
+//       rewrite: (path) => path.replace(/^\/api/, ""),
+//     },
+//   },
+//   cors: false,
+// },
