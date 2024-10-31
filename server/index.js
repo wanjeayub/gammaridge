4c5188c4d15419b297abb15d6b1872f0f7be6274
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-31 17:20:00 +0300

    resolve typo

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ var __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "*","https://gammaridge.netlify.app",
+    origin: ["*", "https://gammaridge.netlify.app"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
