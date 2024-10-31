1e4e0eb3010d1eba3798a49a1066caa4c390e065
Ayub Wanje <ayub.wanje@gmail.com>
2024-10-31 16:49:14 +0300

    update cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ var __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: ["*", "https://gammaridge.netlify.app"],
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
   })
 );
 
