1a13a8a7640e9bd9748ad4af708f8bbd4b1f7090
Ayub Wanje <ayub.wanje@gmail.com>
2024-11-01 23:44:41 +0300

    change url for testing

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -8,6 +8,10 @@ const {
 const { protect } = require("../middleware/authMiddleware.js");
 
 const router = express.Router();
+// url testing
+router.get("/testing", (req, res) => {
+  res.status(200).json(message, "working okay");
+});
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
