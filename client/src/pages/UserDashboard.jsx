7710021f785770f88e9e857543d0f889c102f0da
Ayub Wanje <ayub.wanje@gmail.com>
2024-11-03 01:09:31 +0300

    update user cannot apply if loan status is pending

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -47,44 +47,52 @@ const UserDashboard = () => {
 
   return (
     <div className="container mx-auto p-3 text-white">
-      <h1 className="text-2xl font-bold">User Dashboard</h1>
-      <div className="flex flex-col gap-4">
-        <h2 className="text-xl">Apply for a new loan</h2>
-        <form onSubmit={handleSubmit} className="max-w-md gap-3 flex">
-          <input
-            name="amount"
-            value={loanData.amount}
-            onChange={handleChange}
-            placeholder="Amount"
-            required
-            className="border p-2 w-full bg-gray-600 text-white"
-          />
+      <div>
+        <h1 className="text-2xl font-bold">User Dashboard</h1>
+        {loans.length > 1 && loans.status === "pending" ? (
+          <p>Your loan is not yet approved</p>
+        ) : (
+          <div className="flex flex-col gap-4">
+            <h2 className="text-xl">Apply for a new loan</h2>
+            <form onSubmit={handleSubmit} className="max-w-md gap-3 flex">
+              <input
+                name="amount"
+                value={loanData.amount}
+                onChange={handleChange}
+                placeholder="Amount"
+                required
+                className="border p-2 w-full bg-gray-600 text-white"
+              />
 
-          <button
-            type="submit"
-            className="bg-[#b9283b] text-white py-2 px-4 w-full"
-          >
-            Apply
-          </button>
-        </form>
+              <button
+                type="submit"
+                className="bg-[#b9283b] text-white py-2 px-4 w-full"
+              >
+                Apply
+              </button>
+            </form>
+          </div>
+        )}
       </div>
 
-      <h2 className="text-xl mt-8">My Loans</h2>
-      {loans.length === 0 ? (
-        <p>You have no running loans</p>
-      ) : (
-        <ul className="flex p-4 flex-col">
-          {loans.map((loan) => (
-            <li key={loan._id} className="mb-2">
-              Amount: <span className="font-semibold">Ksh</span> {loan.amount} -
-              Interest: <span className="font-semibold">Ksh</span>
-              {loan.interest} - Total Amount:
-              <span className="font-semibold"> Ksh</span> {loan.totalLoan} -
-              Status: {loan.status}
-            </li>
-          ))}
-        </ul>
-      )}
+      <div>
+        <h2 className="text-xl mt-8">My Loans</h2>
+        {loans.length === 0 ? (
+          <p>You have no running loans</p>
+        ) : (
+          <div className="flex p-4 flex-col">
+            {loans.map((loan) => (
+              <div key={loan._id} className="mb-2 border">
+                Amount: <span className="font-semibold">Ksh</span> {loan.amount}{" "}
+                - Interest: <span className="font-semibold">Ksh</span>
+                {loan.interest} - Total Amount:
+                <span className="font-semibold"> Ksh</span> {loan.totalLoan} -
+                Status: {loan.status}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
