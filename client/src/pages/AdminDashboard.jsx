b6b708244c88d65e5104ec21079922165f7b7835
Ayub Wanje <ayub.wanje@gmail.com>
2024-11-26 22:09:46 +0300

    add axios import

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 // import ShowSpecialLoans from "../specialLoans/ShowSpecialLoans";
 
 const AdminDashboard = () => {
