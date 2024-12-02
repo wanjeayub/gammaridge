6ea62e352f64b79e7cb004afe47d5fffad73eb65
Ayub Wanje <ayub.wanje@gmail.com>
2024-12-02 14:58:57 +0300

    update cors after failure

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ var __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: ["*", "https://gammaridge.vercel.app", "http://localhost:5173"],
+    origin: ["*", "https://gammaridge.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
