c6b6766edd876ee839f4e14dadc2fefb77ea6bf8
Ayub Wanje <ayub.wanje@gmail.com>
2024-12-27 08:30:22 +0300

    adjust max width for laptop screens

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -28,7 +28,7 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="max-w-md mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4 text-white">Update Password</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
